17c3ae56bb540502fbb4ceaf72def112e145ca77
Emily Armstrong <emarmstrong2@gmail.com>
2024-10-01 20:03:55 +0100

    Implemented scikit-learn linear regression model

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import pandas as pd
+from sklearn import linear_model
 
 df = pd.read_csv('dataset.csv')
 
-print(df.head())
+X = df[["acceleration_x","acceleration_y","acceleration_z","gyro_x","gyro_y","gyro_z"]]
+Y = df[["activity"]]
+
+regressor = linear_model.LinearRegression()
+regressor.fit(X, Y)
+
+test_data = pd.DataFrame([[0.5972, -1.1432, -0.2235, 0.4744, -1.1092, -2.9296]],
+                         columns=["acceleration_x", "acceleration_y", "acceleration_z", "gyro_x", "gyro_y", "gyro_z"])
+prediction = regressor.predict(test_data)
+
+if prediction < 0.5:
+    print("Walk")
+elif prediction >= 0.5 and prediction <= 1:
+    print("Run")
+else:
+    print("Error")
+
+print(prediction)
+
