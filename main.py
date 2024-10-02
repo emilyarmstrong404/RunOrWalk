81257fda974518e5cbce2bed98ba64b39ed49161
Emily Armstrong <emarmstrong2@gmail.com>
2024-10-02 22:07:17 +0100

    Functions for computing cost and gradient to use in gradient descent

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import pandas as pd
-from sklearn import linear_model
+import numpy as np
 
 df = pd.read_csv('dataset.csv')
 
 X = df[["acceleration_x","acceleration_y","acceleration_z","gyro_x","gyro_y","gyro_z"]]
 Y = df[["activity"]]
 
-regressor = linear_model.LinearRegression()
-regressor.fit(X, Y)
+b_init = 0
+w_init = np.zeros(6)
+iterations = 1000
+learning_rate = 0.001
 
-test_data = pd.DataFrame([[0.5972, -1.1432, -0.2235, 0.4744, -1.1092, -2.9296]],
-                         columns=["acceleration_x", "acceleration_y", "acceleration_z", "gyro_x", "gyro_y", "gyro_z"])
-prediction = regressor.predict(test_data)
+def compute_cost(x, y, w, b):
+    m = x.shape[0]
+    total_cost = 0
+    i = 0
+    for i in range(m):
+        unsqcost = w * x[i] + b - y[i]
+        sqcost = unsqcost * unsqcost
+        total_cost += sqcost
+    total_cost = total_cost / (2 * m)
+    return total_cost
 
-if prediction < 0.5:
-    print("Walk")
-elif prediction >= 0.5 and prediction <= 1:
-    print("Run")
-else:
-    print("Error")
-
-print(prediction)
-
+def compute_gradient(x, y, w, b):
+    m = x.shape[0]
+    dj_dw = 0
+    dj_db = 0
+    i = 0
+    for i in range(m):
+        prediction = w * x[i] + b
+        dj_db += prediction - y[i]
+        dj_dw += (prediction - y[i]) * x[i]
+    dj_db = dj_db / m
+    dj_dw = dj_dw / m
+    return dj_dw, dj_db
