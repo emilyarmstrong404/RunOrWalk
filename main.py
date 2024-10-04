fe25e6532201c12a2d04661d55115ecfc770e213
Emily Armstrong <emarmstrong2@gmail.com>
2024-10-04 17:38:33 +0100

    Vectorisation

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import numpy as np
 
 df = pd.read_csv('dataset.csv')
 
+df[["acceleration_x","acceleration_y","acceleration_z","gyro_x","gyro_y","gyro_z"]] = df[["acceleration_x","acceleration_y","acceleration_z","gyro_x","gyro_y","gyro_z"]].astype(np.float32)
+df["activity"] = df["activity"].astype(np.float32)
+
 X = df[["acceleration_x","acceleration_y","acceleration_z","gyro_x","gyro_y","gyro_z"]]
 Y = df[["activity"]]
 
@@ -13,25 +16,19 @@ learning_rate = 0.001
 
 def compute_cost(x, y, w, b):
     m = x.shape[0]
-    total_cost = 0
-    i = 0
-    for i in range(m):
-        unsqcost = w * x[i] + b - y[i]
-        sqcost = unsqcost * unsqcost
-        total_cost += sqcost
-    total_cost = total_cost / (2 * m)
+    predictions = np.dot(x, w) + b
+    errors = predictions - y.values.flatten()
+    total_cost = (1 / (2 * m)) * np.sum(errors ** 2)
     return total_cost
 
 def compute_gradient(x, y, w, b):
+    x = x.values
+    y = y.values
     m = x.shape[0]
-    dj_dw = 0
-    dj_db = 0
-    for i in range(m):
-        prediction = w * x[i] + b
-        dj_db += prediction - y[i]
-        dj_dw += (prediction - y[i]) * x[i]
-    dj_db = dj_db / m
-    dj_dw = dj_dw / m
+    predictions = np.dot(x, w) + b
+    errors = predictions - y.flatten()
+    dj_dw = (1 / m) * np.dot(x.T, errors)
+    dj_db = (1 / m) * np.sum(errors)
     return dj_dw, dj_db
 
 def gradient_descent(x, y, w_in, b_in, gradient_function, alpha, num_iters):
@@ -44,4 +41,11 @@ def gradient_descent(x, y, w_in, b_in, gradient_function, alpha, num_iters):
         w = w - alpha * dj_dw
         b = b - alpha * dj_db
 
+        if i % 100 == 0: 
+            cost = compute_cost(x, y, w, b)
+            print(f"Iteration {i}, Cost: {cost}")
+
     return w, b
+
+w,b = gradient_descent(X ,Y, w_init, b_init, compute_gradient, learning_rate, iterations)
+print("w,b found by gradient descent, w: ", w, "b ", b)
