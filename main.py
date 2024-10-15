f518bddbea05f23580eb248ff064ecf5c9ff8a9a
Emily Armstrong <emarmstrong2@gmail.com>
2024-10-15 12:25:54 +0100

    Neural network 99% accuracy

--- main.py
+++ main.py
@@ -1,91 +1,33 @@
 import pandas as pd
-import numpy as np
 from sklearn.preprocessing import StandardScaler
-s = StandardScaler()
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-rfc=RandomForestClassifier()
-from sklearn.metrics import confusion_matrix,accuracy_score,classification_report
+import tensorflow as tf
+
 
 df = pd.read_csv('dataset.csv')
 
 df.drop(columns=['date', 'time', 'username', 'wrist'], inplace=True)
 
-df[["acceleration_x","acceleration_y","acceleration_z","gyro_x","gyro_y","gyro_z"]] = df[["acceleration_x","acceleration_y","acceleration_z","gyro_x","gyro_y","gyro_z"]].astype(np.float32)
-df["activity"] = df["activity"].astype(np.float32)
-
 X = df[["acceleration_x","acceleration_y","acceleration_z","gyro_x","gyro_y","gyro_z"]]
 Y = df[["activity"]].values.flatten()
 
-X = s.fit_transform(X)
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
 X = pd.DataFrame(X)
 
-X_train, X_test,Y_train, Y_test = train_test_split(X,Y,test_size=0.25,random_state=10)
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.25,random_state=10)
 
-rfc.fit(X_train,Y_train)
-Y_pred = rfc.predict(X_test)
+neural_model = tf.keras.Sequential()
+neural_model.add(tf.keras.Input(shape=(6,)))
+neural_model.add(tf.keras.layers.Dense(10, activation='relu'))
+neural_model.add(tf.keras.layers.Dense(5, activation='relu'))
+neural_model.add(tf.keras.layers.Dense(1, activation='sigmoid')) #sigmoid as binary output (walk or run)
 
-print(confusion_matrix(y_pred=Y_pred,y_true=Y_test))
-print(accuracy_score(y_pred=Y_pred,y_true=Y_test))
-print(classification_report(y_pred=Y_pred,y_true=Y_test))
+neural_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']) #binary_crossentropy as binary classification
 
+train_history = neural_model.fit(X_train, Y_train, epochs=100, batch_size=32)
 
-'''
-b_init = 0
-w_init = np.zeros(6)
-iterations = 3000
-learning_rate = 0.01
-
-def compute_cost(x, y, w, b):
-    m = x.shape[0]
-    predictions = np.dot(x, w) + b
-    errors = predictions - y.values.flatten()
-    total_cost = (1 / (2 * m)) * np.sum(errors ** 2)
-    return total_cost
-
-def compute_gradient(x, y, w, b):
-    x = x.values
-    y = y.values
-    m = x.shape[0]
-    predictions = np.dot(x, w) + b
-    errors = predictions - y.flatten()
-    dj_dw = (1 / m) * np.dot(x.T, errors)
-    dj_db = (1 / m) * np.sum(errors)
-    return dj_dw, dj_db
-
-def gradient_descent(x, y, w_in, b_in, gradient_function, alpha, num_iters):
-    w = w_in
-    b = b_in
-
-    for i in range(num_iters):
-        dj_dw, dj_db = gradient_function(x, y, w, b)
-
-        w = w - alpha * dj_dw
-        b = b - alpha * dj_db
-
-        if i % 100 == 0:
-            cost = compute_cost(x, y, w, b)
-            print(f"Iteration {i}, Cost: {cost}")
-
-    return w, b
-
-w,b = gradient_descent(X ,Y, w_init, b_init, compute_gradient, learning_rate, iterations)
-print("w,b found by gradient descent, w: ", w, "b ", b)
-
-m = X.shape[0]
-accuracy_counter = 0
-for i in range(m):
-    if np.dot(X.iloc[i], w + b) < 0.5:
-        walk_or_run = "walk"
-    else:
-        walk_or_run = "run"
-
-    if int(Y.iloc[i]) < 0.5:
-        target_walk_or_run = "walk"
-    else:
-        target_walk_or_run = "run"
-    print(f"prediction: {np.dot(X.iloc[i], w + b):0.2f}, target value: {Y.iloc[i]}, prediction: {walk_or_run}, target: {target_walk_or_run}" )
-    if walk_or_run == target_walk_or_run:
-        accuracy_counter += 1
-print("Accuracy: ", accuracy_counter/m)
-'''
+loss, accuracy = neural_model.evaluate(X_test, Y_test)
+print(f'Test model loss: {round(loss,4)}')
+print(f'Test model accuracy: {round(100*accuracy,2)}%')
